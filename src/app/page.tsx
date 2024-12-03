9ea78f616cc345732b9b0260a9f37a0cc6339cb1
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-12-03 23:06:48 +0100

    UPDATE : Clean + new header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 export default function Home() {
   return (
-    <div>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <p>{"Coucou cha' <3"}</p>
     </div>
   );
