bbc81a9647ce7b89f48159305f428e5b4b88cbe4
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-12-04 00:59:50 +0100

    UPDATE : Update withlove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,12 @@ export default function Home() {
     >
       <p>{"Coucou cha <3"}</p>
       <p>{"Je t'aime"}</p>
+      <p>Je pense à toi</p>
+      <p style={{
+          marginTop: "20px",
+      }}>
+        <strong>Signé:</strong> Ton homme
+      </p>
       <div>
         <img
           src="https://i.pinimg.com/originals/01/6c/c8/016cc8a0e4c93e07a1063b485dcc221a.gif"
