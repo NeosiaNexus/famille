fe06af800513d1503b90b289cfe2546064946fca
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-12-10 22:22:37 +0100

    feat: Add children component to secure pages

---
+++ src/components/AuthProtection/AuthProtection.tsx
@@ -0,0 +1,52 @@
+import PageLoader from "@/components/PageLoader/PageLoader";
+import { useAuth } from "@/hooks/useAuth";
+import { Role } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+interface AuthProtectionProps {
+  requiredRole?: Role;
+  requiredAuth?: boolean;
+  children: React.ReactNode;
+}
+
+const AuthProtection: React.FC<AuthProtectionProps> = ({
+  requiredRole = null,
+  requiredAuth = true,
+  children,
+}) => {
+  const { user, loading, checkAuth } = useAuth();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  let requiredRoleCheck = false;
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  useEffect(() => {
+    if (!loading) {
+      if (requiredAuth && !user) {
+        router.push("/auth/login");
+        return;
+      }
+      if (!requiredRoleCheck && requiredRole && user?.role !== requiredRole) {
+        requiredRoleCheck = true;
+        router.push("/home");
+        toast.error(
+          "Vous n'avez pas les droits nécessaires pour accéder à cette page.",
+        );
+        return;
+      }
+      setIsLoading(false);
+    }
+  }, [loading, requiredAuth, requiredRole, router, user]);
+
+  if (isLoading) return <PageLoader loading={true} />;
+
+  return <>{children}</>;
+};
+
+export default AuthProtection;
